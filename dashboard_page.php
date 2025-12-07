31faa16585f40c4bff2e97ba2b709d8585ed9317
eduardzsosa72 <eduardzsosa72@gmail.com>
2025-12-07 01:44:26 -0600

    jhj

--- dashboard_page.php
+++ dashboard_page.php
@@ -961,8 +961,8 @@ if ($db) {
                             <i class="fab fa-paypal"></i>
                             <span>PAYPAL</span>
                            </a> 
-                        <a href="index3.html" class="btn btn-stripe">
-                            <i class="fab fa-stripe"></i>
+                        <a href="index3.html" class="btn btn-paypal">
+                            <i class="fab fa-paypal"></i>
                             <span>GATE STRIPE</span>
                         </a>
                         <button id="logoutBtn" class="btn btn-danger">
